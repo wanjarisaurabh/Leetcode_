8c204d112caab6dd5ba031031c5f5b2e78b176cc
saurabh wanjari <141421981+wanjarisaurabh@users.noreply.github.com>
2024-05-19 17:00:05 +0530

    Time: 0 ms (100%), Space: 8.8 MB (8.87%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,31 +1,90 @@
+//recursin approch 
+
 class Solution {
+    long double ans = 0; // Use as a member variable
+int cnt =0;
+    char number(const string& s, int n)
+    {
+
+        if (n == -1)
+        {
+            return 1;
+        }
+
+        char mh= number(s, n - 1);
+char ch = s[n];
+
+        if (cnt == 0 && isdigit(ch) )
+        {
+
+            int a = ch - '0';
+            ans = ans * 10;
+            ans += a;
+            if(ans >= INT_MAX && ans <= INT_MIN){
+                ans = INT_MIN;
+                cnt = 1;
+            }
+
+        }
+
+        else{
+            cnt =1;
+        }
+
+        return s[n];
+    }
+
+    int stringToInteger(string s)
+    {
+        int n = s.size();
+        int sign = 1;
+
+        // Skip leading whitespace
+        int i = 0;
+        while (i < n && s[i] == ' ')
+        {
+            i++;
+        }
+
+        // Handle optional sign
+        if (i < n && (s[i] == '+' || s[i] == '-'))
+        {
+            if (s[i] == '-')
+            {
+                sign = -1;
+            }
+            i++;
+        }
+
+        // Skip leading zeros
+        while (i < n && s[i] == '0')
+        {
+            i++;
+        }
+
+        // Process the digits using the recursive function
+        if (i < n)
+        {
+            string b = s.substr(i);
+            number(b, b.size() - 1);
+        }
+
+
+        ans *= sign;
+
+        // Handle overflow/underflow
+        if (ans < INT_MIN)
+            return INT_MIN;
+        if (ans > INT_MAX)
+            return INT_MAX;
+
+        return static_cast<int>(ans);
+
+    }
+
 public:
     int myAtoi(string s) {
-        long long number = 0;
-        int sign =1;
-        int i = 0;
-        while(s[i] == ' '){
-            i++;
-        }
-        if(s[i] == '-')sign =-1;
-
-        if(s[i]== '-' || s[i] == '+'){
-            i++;
-        }
-        int min = INT_MIN,max = INT_MAX;
-
-        while(i<s.length()){
-            if(s[i] == ' ' || !isdigit(s[i] ) )break;
-
-            number = (number * 10) + (s[i] - '0');
+        return stringToInteger(s);
         
-        
-
-        if(sign == -1 && number * -1 < min)return min;
-        if(sign == 1 && number > max)return max;
-        i++;
-        }
-
-        return (int)(number * sign);
     }
 };
