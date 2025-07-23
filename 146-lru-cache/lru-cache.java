f9f84dc85170ecedfea06dfa8aabf65c53686234
saurabh wanjari <141421981+wanjarisaurabh@users.noreply.github.com>
2025-07-23 23:16:42 +0530

    Time: 54 ms (17.89%) | Memory: 127.8 MB (9.97%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,61 @@
+class LRUCache {
+    class Node {
+        int key, value;
+        Node prev, next;
+        Node(int k, int v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    private int capacity;
+    private Map<Integer, Node> map;
+    private Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insert(Node node) {
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+
+        Node node = map.get(key);
+        remove(node);
+        insert(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            remove(map.get(key));
+            map.remove(key);
+        }
+
+        if (map.size() == capacity) {
+            Node lru = tail.prev;
+            remove(lru);
+            map.remove(lru.key);
+        }
+
+        Node newNode = new Node(key, value);
+        insert(newNode);
+        map.put(key, newNode);
+    }
+}
