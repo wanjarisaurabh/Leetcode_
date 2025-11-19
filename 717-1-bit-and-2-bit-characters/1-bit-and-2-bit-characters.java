0bbf73bcb6535f5c657768da79d241e7ebf4b00a
saurabh wanjari <141421981+wanjarisaurabh@users.noreply.github.com>
2025-11-19 22:40:07 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (25.42%) - LeetSync

--- 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.java
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.java
@@ -1,15 +1,34 @@
-class Solution {
-    public boolean isOneBitCharacter(int[] bits) {
-        int n = bits.length;    
-        int count = 0;
+// class Solution {
+//     public boolean isOneBitCharacter(int[] bits) {
+//         int n = bits.length;    
+//         int count = 0;
 
-        // Count trailing 1s before the last bit
-        for (int i = n - 2; i >= 0 && bits[i] == 1; i--) {
-            count++;
+//         // Count trailing 1s before the last bit
+//         for (int i = n - 2; i >= 0 && bits[i] == 1; i--) {
+//             count++;
+//         }
+
+//         // If count of consecutive 1s is even → last bit is one-bit char
+//         // If odd → last bit is part of a two-bit char
+//         return count % 2 == 0;
+//     }
+// }
+
+class Solution {
+    public boolean isOneBitCharacter(int[] bits){
+        int n = bits.length;
+        int i = 0;
+        while(i < n-1){
+            if(bits[i] == 0){
+                i++;
+            }
+
+            if(bits[i] == 1){
+                i = i +2;
+            }
         }
 
-        // If count of consecutive 1s is even → last bit is one-bit char
-        // If odd → last bit is part of a two-bit char
-        return count % 2 == 0;
+
+    return i == n-1;
     }
 }
